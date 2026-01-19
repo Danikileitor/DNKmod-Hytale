921e21abe1d93f3f6f538dcc985404b31c3d0b65
Danikileitor <focusyi@hotmail.com>
2026-01-19 14:51:27 +0100

    inicio

---
+++ src/main/java/dnk/hytalemodding/commands/MainCommands/PlayerInfo.java
@@ -0,0 +1,50 @@
+package dnk.hytalemodding.commands.MainCommands;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractTargetPlayerCommand;
+import com.hypixel.hytale.server.core.entity.UUIDComponent;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+public class PlayerInfo extends AbstractTargetPlayerCommand {
+
+    public PlayerInfo() {
+        super("PlayerInfo", "Get target player info");
+    }
+
+    @Override
+    protected void execute(@Nonnull CommandContext commandContext, @Nullable Ref<EntityStore> targetRef,
+            @Nonnull Ref<EntityStore> ref, @Nonnull PlayerRef playerRef, @Nonnull World world,
+            @Nonnull Store<EntityStore> store) {
+
+        Player player = store.getComponent(ref, Player.getComponentType());
+        UUIDComponent component = store.getComponent(ref, UUIDComponent.getComponentType());
+        TransformComponent transform = store.getComponent(ref, TransformComponent.getComponentType());
+
+        try {
+            if (targetRef == null) {
+                player.sendMessage(Message.raw("Target player not found."));
+                return;
+            }
+        } catch (Exception e) {
+            player.sendMessage(Message.raw("An error occurred while fetching target player."));
+            return;
+        }
+
+        Player targetPlayer = store.getComponent(targetRef, Player.getComponentType());
+        UUIDComponent targetComponent = store.getComponent(targetRef, UUIDComponent.getComponentType());
+        TransformComponent targetTransform = store.getComponent(targetRef, TransformComponent.getComponentType());
+
+        player.sendMessage(Message.raw("UUID: " + targetComponent.getUuid()));
+        player.sendMessage(Message.raw("Position: " + targetTransform.getPosition()));
+    }
+}
