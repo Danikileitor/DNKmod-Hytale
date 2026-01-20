a2c80f6556acfd41dc8c917d2cedb226a88a157a
Danikileitor <focusyi@hotmail.com>
2026-01-20 16:31:42 +0100

    inicio

--- src/main/java/dnk/hytalemodding/Main.java
+++ src/main/java/dnk/hytalemodding/Main.java
@@ -7,17 +7,37 @@ import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import dnk.hytalemodding.commands.MainCommand;
 import dnk.hytalemodding.events.JoinEvent;
 
+import java.util.logging.Level;
+
 import javax.annotation.Nonnull;
 
 public class Main extends JavaPlugin {
+
+    private static Main instance;
 
     public Main(@Nonnull JavaPluginInit init) {
         super(init);
     }
 
+    public static Main get() {
+        return instance;
+    }
+
     @Override
     protected void setup() {
+        instance = this;
         this.getCommandRegistry().registerCommand(new MainCommand());
         this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, JoinEvent::onPlayerReady);
+        getLogger().at(Level.INFO).log("Plugin setup complete!");
+    }
+
+    @Override
+    protected void start() {
+        getLogger().at(Level.INFO).log("Plugin started!");
+    }
+
+    @Override
+    protected void shutdown() {
+        getLogger().at(Level.INFO).log("Plugin shutting down!");
     }
 }
