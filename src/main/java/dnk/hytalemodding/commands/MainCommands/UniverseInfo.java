c9b04a5e5ab3e44dfdcdafe65bdbf234179efc3c
Danikileitor <focusyi@hotmail.com>
2026-01-26 13:14:54 +0100

    UniverseInfo

--- src/main/java/dnk/hytalemodding/commands/MainCommands/UniverseInfo.java
+++ src/main/java/dnk/hytalemodding/commands/MainCommands/UniverseInfo.java
@@ -1,5 +1,6 @@
 package dnk.hytalemodding.commands.MainCommands;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import javax.annotation.Nullable;
@@ -7,6 +8,8 @@ import javax.annotation.Nullable;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.AbstractCommand;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.universe.Universe;
+import com.hypixel.hytale.server.core.universe.world.World;
 
 public class UniverseInfo extends AbstractCommand {
 
@@ -17,7 +20,15 @@ public class UniverseInfo extends AbstractCommand {
     @Override
     @Nullable
     protected CompletableFuture<Void> execute(CommandContext commandContext) {
-        Message.raw("Hello World");
+        Universe universe = Universe.get();
+        Map<String, World> worlds = universe.getWorlds();
+
+        commandContext.sendMessage(Message.translation("server.commands.dnk.universeinfo.worlds.title").param("0", worlds.size()));
+        worlds.forEach((key, world) -> {
+            commandContext.sendMessage(Message.translation("server.commands.dnk.universeinfo.worlds").param("0", world.getName()).param("1", world.getPlayerCount()));
+        });
+        commandContext.sendMessage(Message.raw("Hello World"));
+
         return CompletableFuture.completedFuture(null);
     }
 }
