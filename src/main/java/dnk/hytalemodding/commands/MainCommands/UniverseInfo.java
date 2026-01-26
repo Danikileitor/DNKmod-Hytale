e5a3f04917359a68d6a552231591cc8c8efa9da8
Danikileitor <focusyi@hotmail.com>
2026-01-26 14:23:44 +0100

    UniverseInfo

--- src/main/java/dnk/hytalemodding/commands/MainCommands/UniverseInfo.java
+++ src/main/java/dnk/hytalemodding/commands/MainCommands/UniverseInfo.java
@@ -3,32 +3,51 @@ package dnk.hytalemodding.commands.MainCommands;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
-import javax.annotation.Nullable;
-
+import javax.annotation.Nonnull;
 import com.hypixel.hytale.server.core.Message;
-import com.hypixel.hytale.server.core.command.system.AbstractCommand;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractAsyncCommand;
 import com.hypixel.hytale.server.core.universe.Universe;
 import com.hypixel.hytale.server.core.universe.world.World;
 
-public class UniverseInfo extends AbstractCommand {
+public class UniverseInfo extends AbstractAsyncCommand {
 
     public UniverseInfo() {
         super("universeinfo", "server.commands.dnk.universeinfo.desc");
+        addSubCommand(new UniversePlayerList());
     }
 
     @Override
-    @Nullable
-    protected CompletableFuture<Void> execute(CommandContext commandContext) {
+    protected CompletableFuture<Void> executeAsync(@Nonnull CommandContext commandContext) {
         Universe universe = Universe.get();
-        Map<String, World> worlds = universe.getWorlds();
+        commandContext.sendMessage(Message.translation("server.commands.dnk.universeinfo.name").param("0", universe.getName()));
+        commandContext.sendMessage(Message.translation("server.commands.dnk.universeinfo.playercount").param("0", universe.getPlayerCount()));
+        commandContext.sendMessage(Message.translation("server.commands.dnk.universeinfo.defaultworld").param("0", universe.getDefaultWorld().getName()));
 
+        Map<String, World> worlds = universe.getWorlds();
         commandContext.sendMessage(Message.translation("server.commands.dnk.universeinfo.worlds.title").param("0", worlds.size()));
         worlds.forEach((key, world) -> {
-            commandContext.sendMessage(Message.translation("server.commands.dnk.universeinfo.worlds").param("0", world.getName()).param("1", world.getPlayerCount()));
+            commandContext.sendMessage(Message.translation("server.commands.dnk.universeinfo.worlds.world").param("0", world.getName()).param("1", world.getPlayerCount()));
         });
-        commandContext.sendMessage(Message.raw("Hello World"));
 
         return CompletableFuture.completedFuture(null);
+    }
+
+    public static class UniversePlayerList extends AbstractAsyncCommand {
+
+        public UniversePlayerList() {
+            super("playerlist", "server.commands.dnk.universeinfo.playerlist.desc");
+        }
+
+        @Override
+        protected CompletableFuture<Void> executeAsync(CommandContext commandContext) {
+            Universe universe = Universe.get();
+            commandContext.sendMessage(Message.translation("server.commands.dnk.universeinfo.playerlist.title").param("0", universe.getPlayerCount()));
+            universe.getPlayers().forEach(player -> {
+                commandContext.sendMessage(Message.translation("server.commands.dnk.universeinfo.playerlist.player").param("0", player.getUsername()));
+            });
+
+            return CompletableFuture.completedFuture(null);
+        }
     }
 }
