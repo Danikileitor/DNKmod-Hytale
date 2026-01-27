46492626b64a903995fa089308804d5fcce04357
Danikileitor <focusyi@hotmail.com>
2026-01-27 12:20:36 +0100

    permisos

--- src/main/java/dnk/hytalemodding/commands/MainCommand.java
+++ src/main/java/dnk/hytalemodding/commands/MainCommand.java
@@ -1,6 +1,7 @@
 package dnk.hytalemodding.commands;
 
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
+import com.hypixel.hytale.server.core.permissions.HytalePermissions;
 
 import dnk.hytalemodding.commands.MainCommands.PlayerInfo;
 import dnk.hytalemodding.commands.MainCommands.UniverseInfo;
@@ -9,6 +10,7 @@ public class MainCommand extends AbstractCommandCollection {
 
     public MainCommand() {
         super("dnk", "server.commands.dnk.main.desc");
+        requirePermission(HytalePermissions.fromCommand(this.getName()));
         this.addSubCommand(new PlayerInfo());
         this.addSubCommand(new UniverseInfo());
     }
